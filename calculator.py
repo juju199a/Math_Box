13effad14cdec4331988ef55770514952f92b98a
Youngjun Ju <164507032+juju199a@users.noreply.github.com>
2026-06-21 21:11:02 +0900

    Revert "Premium"

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+# 기존 계산기
 def add(a,b) :
 	return a+b
 
@@ -7,9 +8,6 @@ def subtract(a,b) :
 def multiply(a,b) :
 	return a*b
 
-def divide_new(a, b):
-	return a/b
-
 def divde_free(a,b) :
 	if b == 0: return 0
 	else : return a/b
@@ -18,40 +16,8 @@ def mod(a,b):
 	if b == 0: return 0
 	return a%b
 
-def get_Median(a, b):
-	return (a+b)/2
-
-def get_Remainder(a,b) :
-	return a//b
-
-def get_Abs(num):
-	if num>=0:
-		return num
-	else:
-		return -num
-
-def square(a):
-	return a*a
-
-def get_Percent(a,b):
-	return (a/b) * 100
-
-def get_Sum_ver1(n):
-	return n(n+1)/2
-
-def factorial(n):
-	num = 1
-	while n >= 1:
-		num = num + n
-		n = n - 1
-	return num
-
-def emergency():
-	print("emergency")
-
 def square(a):
 	return a*a
 
 def main_edit():
 	print("main edit")
-
