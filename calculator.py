313e363ef37b9f788a8fa4bc98f4bca82a108c65
Youngjun Ju <juju199a@gmail.com>
2026-06-26 21:53:56 +0900

    fix: 계산기 로직 오류 수정 및 PEP 8 코드 스타일 적용

--- calculator.py
+++ calculator.py
@@ -1,58 +1,70 @@
-def add(a,b) :
-	return a+b
+def add(a, b):
+    return a + b
 
-def subtract(a,b) :
-	return a-b
 
-def multiply(a,b) :
-	return a*b
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
 
 def divide_new(a, b):
-	return a/b
+    return a / b
 
-def divde_free(a,b) :
-	if b == 0: return 0
-	else : return a/b
 
-def mod(a,b):
-	if b == 0: return 0
-	return a%b
+def divde_free(a, b):
+    if b == 0: 
+        return 0
+    else: 
+        return a / b
+
+
+def mod(a, b):
+    if b == 0: 
+        return 0
+    return a % b
+
 
 def get_Median(a, b):
-	return (a+b)/2
+    return (a + b) / 2
 
-def get_Remainder(a,b) :
-	return a//b
+
+def get_Remainder(a, b):
+    return a // b
+
 
 def get_Abs(num):
-	if num>=0:
-		return num
-	else:
-		return -num
+    if num >= 0:
+        return num
+    else:
+        return -num
+
 
 def square(a):
-	return a*a
+    return a * a
 
-def get_Percent(a,b):
-	return (a/b) * 100
+
+def get_Percent(a, b):
+    return (a / b) * 100
+
 
 def get_Sum_ver1(n):
-	return n(n+1)/2
+    return n * (n + 1) / 2
+
 
 def factorial(n):
-	num = 1
-	while n >= 1:
-		num = num + n
-		n = n - 1
-	return num
+    num = 1
+    while n >= 1:
+        num = num * n
+        n = n - 1
+    return num
+
 
 def emergency():
-	print("emergency")
+    print("emergency")
 
-def square(a):
-	return a*a
 
 def main_edit():
-	print("main edit")
-
-
+    print("main edit")
